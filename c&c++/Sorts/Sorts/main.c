175555a5c0ff1503882978b9a885bfd42c85712f
weekbelt4207 <75966556+weekbelt4207@users.noreply.github.com>
2024-11-19 21:13:06 +0900

    MergeSort 구현

--- c&c++/Sorts/Sorts/main.c
+++ c&c++/Sorts/Sorts/main.c
@@ -50,12 +50,103 @@ void SelectionSort(int A[], int n) {
     }
 }
 
+int partition(int A[], int l, int h) {
+    int pivot = A[l];
+    int i=l,j=h;
+    
+    do {
+        do {
+            i++;
+        } while (A[i] <= pivot);
+        
+        do {
+            j--;
+        } while (A[j] > pivot);
+        
+        if (i < j) {
+            swap(&A[i], &A[j]);
+        }
+        
+    } while (i < j);
+    
+    swap(&A[l], &A[j]);
+    
+    return j;
+}
+
+void Merge(int A[], int l, int mid, int h) {
+    int i = l;
+    int j = mid + 1;
+    int k = l;
+    int B[100];
+    
+    while (i <= mid && j <= h) {
+        if (A[i] < A[j]) {
+            B[k++] = A[i++];
+        } else {
+            B[k++] = A[j++];
+        }
+    }
+    
+    while (i <= mid) {
+        B[k++] = A[i++];
+    }
+    
+    while (j <= h) {
+        B[k++] = A[j++];
+    }
+    
+    for (int i=l; i<=h; i++) {
+        A[i] = B[i];
+    }
+}
+
+void IMergeSort(int A[], int n) {
+    int p,l,h,mid,i;
+    
+    for (p=2; p<=n; p=p*2) {
+        for (i=0; i+p-1<n; i=i+p) {
+            l=i;
+            h=i+p-1;
+            mid=(l+h)/2;
+            Merge(A, l, mid, h);
+        }
+    }
+    
+    if (p/2 < n) {
+        Merge(A, 0, p/2 - 1, n);
+    }
+}
+
+void MergeSort(int A[], int l, int h) {
+    int mid;
+    
+    if (l < h) {
+        mid = (l + h) / 2;
+        MergeSort(A, l, mid);
+        MergeSort(A, mid+1, h);
+        Merge(A, l, mid, h);
+    }
+}
+
+void QuickSort(int A[], int l, int h) {
+    int j;
+    
+    if (l<h) {
+        j = partition(A, l, h);
+        QuickSort(A, l, j);
+        QuickSort(A, j+1, h);
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2};
-    int n = 10;
+    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3, 1000000};
+    int n = 11;
     int i;
     
-    SelectionSort(A, n);
+//    QuickSort(A, 0, 10);
+//    IMergeSort(A, n);
+    MergeSort(A, 0, n-1);
     
     for (i=0; i<n; i++) {
         printf("%d ", A[i]);
